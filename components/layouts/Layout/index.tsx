020ae8a152dbe4634c2fcefc8850e7c5ff7c7c17
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-03 12:19:01 +0300

    refactoring

--- components/layouts/Layout/index.tsx
+++ components/layouts/Layout/index.tsx
@@ -3,11 +3,12 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import { View } from 'react-native'
 
-const Layout: FC<PropsWithChildren> = ({children}) => {
+const Layout: FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<SafeAreaProvider>
 			<StatusBar style='auto' />
 			<SafeAreaView className='bg-background-primary flex-1 text-text-primary'>
+				
 				<View className='mx-2 my-4'>
 					{children}
 				</View>
