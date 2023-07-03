6922f4d3498bb8a94a6c88c270eb984ff853c87e
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-03 12:17:55 +0300

    refactoring

--- components/screens/Home/index.tsx
+++ components/screens/Home/index.tsx
@@ -2,14 +2,14 @@ import { FC } from 'react'
 import { View } from 'react-native'
 import Header from './components/Header'
 import Search from './components/Search'
-import Filter from './components/Filter'
+import Cars from './components/Cars'
 
 const Home: FC = () => {
 	return (
 		<View>
 			<Header />
 			<Search />
-			<Filter />
+			<Cars />
 		</View>
 	)
 }
