1e4eb61e378d3e4c03664c941d7aa374d6075a3a
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-03 12:18:46 +0300

    cars list

--- components/screens/Home/components/Cars/index.tsx
+++ components/screens/Home/components/Cars/index.tsx
@@ -1,11 +1,22 @@
-import { View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import { FC } from 'react'
 import useCars from './hooks/useCars'
+import Car from '../Car'
+import Filter from '../Filter'
 
 const Cars: FC = () => {
-	const { cars } = useCars()
+	const { cars, filterCarsByType } = useCars()
 	
-	return <View></View>
+	return <>
+		<Filter filterCallback={filterCarsByType} />
+		<View className='my-6'>
+			<Text className='mb-3 text-2xl font-medium px-4'>Your Cars</Text>
+			<FlatList
+				data={cars}
+				renderItem={(({ item, index }) => <Car key={index} {...item} />)}
+			/>
+		</View>
+	</>
 }
 
 export default Cars
