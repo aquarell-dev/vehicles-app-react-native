f623ff6d79e49b645ab82f8ab533424d6186bc53
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-04 16:49:38 +0300

    settings screen

---
+++ components/screens/Settings/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import { Text } from 'react-native'
+import Layout from '../../layouts/Layout'
+
+const Settings: FC = () => {
+	return (
+		<Layout>
+			<Text>123</Text>
+		</Layout>
+	)
+}
+
+export default Settings
