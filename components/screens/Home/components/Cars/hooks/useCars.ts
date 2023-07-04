ac065ae61fd757c9905810f40b2ebe4b9a19437f
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-04 16:49:09 +0300

    fixed filtering

--- components/screens/Home/components/Cars/hooks/useCars.ts
+++ components/screens/Home/components/Cars/hooks/useCars.ts
@@ -1,5 +1,5 @@
-import { useMemo, useState } from 'react'
-import { FilterCarsByType, TCar } from '../../../../../../types/car.types'
+import { useEffect, useMemo, useState } from 'react'
+import { FilterCarsByTitle, FilterCarsByType, TCar } from '../../../../../../types/car.types'
 import vehicles from '../../../../../../vehicles.json'
 import TruckMarker from '../../../../../../assets/markers/truck.png'
 import CarMarker from '../../../../../../assets/markers/truck.png'
@@ -15,16 +15,32 @@ const useCars = () => {
 	})) as TCar[]
 	
 	const [cars, setCars] = useState<TCar[]>(loadCars())
+	const [carsQuery, setCarsQuery] = useState('')
+	const [filterApplied, setFilterApplied] = useState(false)
 	
 	const allCars = useMemo(() => loadCars(), [])
 	
 	const filterCarsByType: FilterCarsByType = carType => {
-		if (!carType) setCars(allCars)
+		if (!carType) {
+			setCars(allCars)
+			return setCarsQuery('')
+		}
 		
 		setCars(allCars.filter(car => car.carType === carType) as TCar[])
 	}
 	
-	return { cars, filterCarsByType }
+	const filterCarsByTitle: FilterCarsByTitle = title =>
+		setCars(
+			allCars.filter(car => car.title.toLowerCase().includes(title.toLowerCase()))
+		)
+	
+	useEffect(() => {
+		if (carsQuery)
+			filterCarsByTitle(carsQuery)
+	}, [carsQuery])
+	
+	
+	return { cars, carsQuery, setCarsQuery, filterCarsByType, filterCarsByTitle }
 }
 
 export default useCars
