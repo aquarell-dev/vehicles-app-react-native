3d87bc27616a30c4fbe865a72f50fd3797e5be6c
Ilya Ivanov <aquarell.dev@gmail.com>
2023-07-06 17:17:22 +0300

    added font for app name

--- App.tsx
+++ App.tsx
@@ -1,18 +1,35 @@
 import 'react-native-gesture-handler'
-import { FC } from 'react'
+import { FC, useCallback } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import Navigation from './components/Navigators/Navigation'
-import { ToastProvider } from 'react-native-toast-notifications'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import useTranslation from './hooks/useTranslation'
 
+SplashScreen.preventAutoHideAsync()
 
 const App: FC = () => {
+	const [fontsLoaded] = useFonts({
+		'MontserratAlt1': require('./assets/fonts/brand-name/MontserratAlt1-SemiBold.otf'),
+	})
+	
+	useTranslation()
+	
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) {
+			await SplashScreen.hideAsync()
+		}
+	}, [fontsLoaded])
+	
+	if (!fontsLoaded) {
+		return <></>
+	}
+	
 	return (
-		<SafeAreaProvider>
-			<ToastProvider>
-				<StatusBar style='auto' />
-				<Navigation />
-			</ToastProvider>
+		<SafeAreaProvider onLayout={onLayoutRootView}>
+			<StatusBar style='light' />
+			<Navigation />
 		</SafeAreaProvider>
 	)
 }
